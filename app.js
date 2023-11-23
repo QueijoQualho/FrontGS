6c99e9c6725a94546551eb72ebc9e680d3a6ed04
Pedro Jesus <134734704+QueijoQualho@users.noreply.github.com>
2023-11-23 11:59:04 -0300

    update

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static('public'));
+
 app.use('/', router)
 
 app.listen(port, () => {
--- routers/router.js
+++ routers/router.js
@@ -1,5 +1,4 @@
 import express from "express";
-import ibgeRouter from "./ibgeApiRouter.js";
 import axios from 'axios';
 
 const router = express.Router();
@@ -27,6 +26,41 @@ router.get('/tipounidades', async (req, res) => {
   }
 });
 
-router.use('/ibgeApi', ibgeRouter)
+
+router.get('/estabelecimentos', async (req, res) => {
+    const data = fetchEstabeleciomentos()
+})
+
+async function fetchEstabeleciomentos(codUnidade, codEstado, codMunicipio = 0) {
+  const urlDefault = "https://apidadosabertos.saude.gov.br/cnes/estabelecimentos"
+
+  let urlApi;
+
+  if (codMunicipio !== 0) {
+    urlApi = `${urlDefault}/?codigo_tipo_unidade=${codUnidade}&codigo_uf=${codEstado}&limit=20`
+  } else {
+    urlApi = `${urlDefault}/?codigo_tipo_unidade=${codUnidade}&codigo_uf=${codEstado}&codigo_municipio=${codMunicipio}&limit=20`
+  }
+
+  try {
+    const response = await axios.get(urlApi);
+    const data = response.data;
+
+    if (data.estabelecimentos !== null) {
+      const options = data.estabelecimentos.map(e => {
+        nome: e.nome_fantasia
+
+      })
+
+      return options;
+    } else {
+      return null;
+    }
+
+  } catch (error) {
+    console.error('Erro na requisição:', error);
+    res.status(500).json({ error: 'Erro na requisição.' });
+  }
+}
 
 export default router;
