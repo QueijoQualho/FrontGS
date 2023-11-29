c226b93e23ea36a869110a4670c453ed79646286
Pedro Jesus <134734704+QueijoQualho@users.noreply.github.com>
2023-11-29 09:41:17 -0300

    fix if the results is null

--- public/js/nodeFunctions.js
+++ public/js/nodeFunctions.js
@@ -42,61 +42,66 @@ export async function gerarResultados(codUnidade, codEstado, codMunicipio = 0) {
 
     try {
         const response = await fetch(urlApi);
-
+    
         if (!response.ok) {
             throw new Error(`Erro na requisição: ${response.status} - ${response.statusText}`);
         }
-
+    
         const data = await response.json();
-        const sortedResult = data.data.sort((a, b) => a.nome.localeCompare(b.nome));
-
-        sortedResult.forEach(e => {
-            const div = document.createElement('div')
-
-            div.className = "card";
-
-            div.innerHTML = `
-    <div class="card-nome">
-        <h3>${e.nome || 'Não possui'}</h3>
-    </div>
-
-    <div>
-        <h5>Endereço</h5>
-        <p>${e.endereco || 'Não possui'}</p>
-    </div>
-
-    <div>
-        <h5>Número</h5>
-        <p>${e.numero || 'Não possui'}</p>
-    </div>
-
-    <div>
-        <h5>Bairro</h5>
-        <p>${e.bairro || 'Não possui'}</p>
-    </div>
-
-    <div>
-        <h5>CEP</h5>
-        <p>${e.cep || 'Não possui'}</p>
-    </div>
-
-    <div>
-        <h5>Telefone</h5>
-        <p>${e.telefone || 'Não possui'}</p>
-    </div>
-
-    <div>
-        <h5>Email</h5>
-        <p>${e.email || 'Não possui'}</p>
-    </div>
-`;
-
-
-            results.appendChild(div)
-
-        });
-
+    
+        if (data.data != null && data.data.length > 0) {
+            const sortedResult = data.data.sort((a, b) => a.nome.localeCompare(b.nome));
+    
+            sortedResult.forEach(e => {
+                const div = document.createElement('div');
+    
+                div.className = "card";
+    
+                div.innerHTML = `
+                    <div class="card-nome">
+                        <h3>${e.nome || 'Não possui'}</h3>
+                    </div>
+    
+                    <div>
+                        <h5>Endereço</h5>
+                        <p>${e.endereco || 'Não possui'}</p>
+                    </div>
+    
+                    <div>
+                        <h5>Número</h5>
+                        <p>${e.numero || 'Não possui'}</p>
+                    </div>
+    
+                    <div>
+                        <h5>Bairro</h5>
+                        <p>${e.bairro || 'Não possui'}</p>
+                    </div>
+    
+                    <div>
+                        <h5>CEP</h5>
+                        <p>${e.cep || 'Não possui'}</p>
+                    </div>
+    
+                    <div>
+                        <h5>Telefone</h5>
+                        <p>${e.telefone || 'Não possui'}</p>
+                    </div>
+    
+                    <div>
+                        <h5>Email</h5>
+                        <p>${e.email || 'Não possui'}</p>
+                    </div>
+                `;
+    
+                results.appendChild(div);
+            });
+        } else {
+            const h2 = document.createElement('h2');
+            h2.textContent = 'Nenhum estabelecimento foi encontrado.';
+            results.appendChild(h2);
+        }
     } catch (error) {
         console.error('Erro na requisição:', error.message);
     }
+    
 }
